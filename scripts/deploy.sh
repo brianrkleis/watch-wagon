6c4779cb66883dadc5f785594215cb96d2fc0456
brianrkleis <brian.kleis@gmail.com>
2023-11-19 16:40:31 -0300

    teste deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ REMOTE_PATH="$1"
 SSH_CONFIG_FILE="${REMOTE_PATH}/ssh-config"
 DOCKER_COMPOSE_FILE=="${REMOTE_PATH}/docker-compose.yml"
 REMOTE_REPO_URL="git@github.com:brianrkleis/watch-wagon.git"
+echo $REMOTE_PATH
 
 # Cópia do docker-compose.yml e ssh-config para o servidor remoto
 scp -o StrictHostKeyChecking=no -i $EC2_SSH_KEY $DOCKER_COMPOSE_FILE ec2-user@${EC2_HOST}:${REMOTE_PATH}
