109d7c8d1a6fcbc8c3f2b9205e4e324e295b34ad
brianrkleis <brian.kleis@gmail.com>
2023-11-19 16:19:57 -0300

    teste deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Configuração
-DOCKER_COMPOSE_FILE="docker-compose.yml"
-SSH_CONFIG_FILE="ssh-config"
-REMOTE_PATH="/path/to/remote/directory"
+REMOTE_PATH="$1"
+SSH_CONFIG_FILE="${REMOTE_PATH}/ssh-config"
+DOCKER_COMPOSE_FILE=="${REMOTE_PATH}/docker-compose.yml"
+REMOTE_REPO_URL="git@github.com:brianrkleis/watch-wagon.git"
 
-echo $(pwd)
+# Cópia do docker-compose.yml e ssh-config para o servidor remoto
+scp -o StrictHostKeyChecking=no -i $EC2_SSH_KEY $DOCKER_COMPOSE_FILE ec2-user@${EC2_HOST}:${REMOTE_PATH}
+scp -o StrictHostKeyChecking=no -i $EC2_SSH_KEY $SSH_CONFIG_FILE ec2-user@${EC2_HOST}:~/.ssh/config
 
-# # Cópia do docker-compose.yml e ssh-config para o servidor remoto
-# scp -o StrictHostKeyChecking=no -i $EC2_SSH_KEY $DOCKER_COMPOSE_FILE ec2-user@${EC2_HOST}:${REMOTE_PATH}
-# scp -o StrictHostKeyChecking=no -i $EC2_SSH_KEY $SSH_CONFIG_FILE ec2-user@${EC2_HOST}:~/.ssh/config
-
-# # SSH no servidor remoto e executa o docker-compose up
-# ssh -i $EC2_SSH_KEY ec2-user@${EC2_HOST} "cd ${REMOTE_PATH} && docker-compose up -d"
+# SSH no servidor remoto e executa o docker-compose up
+ssh -i $EC2_SSH_KEY ec2-user@${EC2_HOST} "cd ${REMOTE_PATH} && git clone ${REMOTE_REPO_URL} && cd watch-wagon && chmod +x /script/start.sh && /script/start.sh"
