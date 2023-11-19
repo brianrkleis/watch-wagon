34a94fa8e1f7ac9b50a6a25b33f83df575816d48
brianrkleis <brian.kleis@gmail.com>
2023-11-19 17:13:08 -0300

    teste deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-REMOTE_PATH="$1"
-SSH_CONFIG_FILE="${REMOTE_PATH}/ssh-config"
-DOCKER_COMPOSE_FILE=="${REMOTE_PATH}/docker-compose.yml"
-REMOTE_REPO_URL="git@github.com:brianrkleis/watch-wagon.git"
-echo $REMOTE_PATH
-
 # SSH no servidor remoto e executa o docker-compose up
 ssh -i $EC2_SSH_KEY ec2-user@${EC2_HOST} "cd ${REMOTE_PATH} && git clone ${REMOTE_REPO_URL} && cd watch-wagon && chmod +x /script/start.sh && bash /script/deploy.sh"
